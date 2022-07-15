3a806d4fdc697634190a6439121df3fc53d623cc
inokawa <48897392+inokawa@users.noreply.github.com>
2022-07-15 12:00:28 +0900

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,44 +1,32 @@
 import typescript from "@rollup/plugin-typescript";
 import pkg from "./package.json";
 
-const plugins = [
-  typescript({
-    tsconfig: "./tsconfig.json",
-    outDir: ".",
-    declaration: true,
-    exclude: ["src/**/*.spec.*"],
-  }),
+const externals = [
+  ...Object.keys(pkg.dependencies),
+  ...Object.keys(pkg.devDependencies),
 ];
 
-export default [
-  {
-    input: "src/index.ts",
-    output: [
-      {
-        file: pkg.main,
-        format: "cjs",
-      },
-      {
-        file: pkg.module,
-        format: "es",
-      },
-    ],
-    external: Object.keys(pkg.dependencies),
-    plugins: plugins,
-  },
-  {
-    input: "src/node.ts",
-    output: [
-      {
-        file: "lib/node.mjs",
-        format: "cjs",
-      },
-      {
-        file: "lib/node.js",
-        format: "es",
-      },
-    ],
-    external: Object.keys(pkg.dependencies),
-    plugins: plugins,
-  },
-];
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
+      format: "es",
+      sourcemap: true,
+    },
+  ],
+  external: (id) => externals.some((d) => id.startsWith(d)),
+  plugins: [
+    typescript({
+      tsconfig: "./tsconfig.json",
+      outDir: ".",
+      declaration: true,
+      exclude: ["**/*.{spec,stories}.*"],
+    }),
+  ],
+};
