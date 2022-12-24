4da3613266c7c48070738c08eb81a7a6162062c0
inokawa <48897392+inokawa@users.noreply.github.com>
2022-12-24 19:50:22 +0900

    Revert "Update rollup.config.js"
    This reverts commit 3a806d4fdc697634190a6439121df3fc53d623cc.

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,44 @@
 import typescript from "@rollup/plugin-typescript";
 import pkg from "./package.json";
 
-const externals = [
-  ...Object.keys(pkg.dependencies),
-  ...Object.keys(pkg.devDependencies),
+const plugins = [
+  typescript({
+    tsconfig: "./tsconfig.json",
+    outDir: ".",
+    declaration: true,
+    exclude: ["src/**/*.spec.*"],
+  }),
 ];
 
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: pkg.main,
-      format: "cjs",
-      sourcemap: true,
-    },
-    {
-      file: pkg.module,
-      format: "es",
-      sourcemap: true,
-    },
-  ],
-  external: (id) => externals.some((d) => id.startsWith(d)),
-  plugins: [
-    typescript({
-      tsconfig: "./tsconfig.json",
-      outDir: ".",
-      declaration: true,
-      exclude: ["**/*.{spec,stories}.*"],
-    }),
-  ],
-};
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: pkg.main,
+        format: "cjs",
+      },
+      {
+        file: pkg.module,
+        format: "es",
+      },
+    ],
+    external: Object.keys(pkg.dependencies),
+    plugins: plugins,
+  },
+  {
+    input: "src/node.ts",
+    output: [
+      {
+        file: "lib/node.mjs",
+        format: "cjs",
+      },
+      {
+        file: "lib/node.js",
+        format: "es",
+      },
+    ],
+    external: Object.keys(pkg.dependencies),
+    plugins: plugins,
+  },
+];
