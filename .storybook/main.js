b7997a2cd40d7e6cd31a108b68699661c1f397e8
inokawa <48897392+inokawa@users.noreply.github.com>
2023-05-14 10:50:47 +0900

    Upgrade storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,4 +4,8 @@ module.exports = {
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
 };
