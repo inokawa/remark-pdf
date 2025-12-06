3874dac0c8188d5507988e8f1e64b5d17a43241f
inokawa <48897392+inokawa@users.noreply.github.com>
2025-12-06 15:56:20 +0900

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -16,10 +16,13 @@ export const isBrowser = () => {
   }
 };
 
-export function deepMerge(target: any, source: any): any {
+export function deepMerge<T extends object>(
+  target: T,
+  source: Partial<T> | undefined
+): T {
   for (const key in source) {
     if (source[key] instanceof Object && key in target) {
-      Object.assign(source[key], deepMerge(target[key], source[key]));
+      Object.assign(source[key], deepMerge(target[key] as object, source[key]));
     }
   }
   return { ...target, ...source };
