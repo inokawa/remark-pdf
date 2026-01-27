082bbe94558e03b898a2af683194ac4fee4bd058
inokawa <48897392+inokawa@users.noreply.github.com>
2026-01-27 09:36:48 +0900

    Fix error with png-js

--- src/shim/zlib/browser.ts
+++ src/shim/zlib/browser.ts
@@ -6,4 +6,12 @@ export default {
   deflateSync: (input: ArrayBuffer) => {
     return Buffer.from(pako.deflate(input));
   },
+  inflate: (buffer: Buffer, cb: (error: Error | null, result: NonSharedBuffer) => void) => {
+    try {
+      const result = pako.inflate(buffer);
+      cb(null, Buffer.from(result));
+    } catch (err) {
+      cb(err as Error, null!)
+    }
+  }
 };
